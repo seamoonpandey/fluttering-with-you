c4d475136048849687e322bc63cc7c092c8e4056
Seamoon Pandey <hackedasocial@gmail.com>
2023-07-26 05:15:19 +0545

    begin

---
+++ lib/dice_roller.dart
@@ -0,0 +1,44 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+class DiceRoller extends StatefulWidget {
+  const DiceRoller({super.key});
+
+  @override
+  State<DiceRoller> createState() {
+    return _DiceRollerState();
+  }
+}
+
+class _DiceRollerState extends State<DiceRoller> {
+  final randomize = Random();
+  var activeDice = 1;
+
+  void rollDice() {
+    setState(() {
+      activeDice = randomize.nextInt(6) + 1;
+    });
+  }
+
+  @override
+  Widget build(context) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Image.asset(
+          'assets/images/dice-$activeDice.png',
+          width: 200,
+        ),
+        TextButton(
+          onPressed: rollDice,
+          style: TextButton.styleFrom(
+              padding: const EdgeInsets.only(top: 20),
+              foregroundColor: Colors.white,
+              textStyle: const TextStyle(fontSize: 32)),
+          child: const Text('Roll'),
+        )
+      ],
+    );
+  }
+}
