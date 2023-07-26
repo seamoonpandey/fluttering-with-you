b740761eb7257e057292fce7764d5f16cc207ba1
Seamoon Pandey <hackedasocial@gmail.com>
2023-07-26 06:21:50 +0545

    outputtnig conditional content

--- lib/quiz.dart
+++ lib/quiz.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_quiz_app/questions_screen.dart';
 import 'package:flutter_quiz_app/start_screen.dart';
 
 class Quiz extends StatefulWidget {
@@ -9,6 +10,22 @@ class Quiz extends StatefulWidget {
 }
 
 class _QuizState extends State<Quiz> {
+  var activeScreen = 'start-screen';
+  //Widget? activeScreen;
+
+  //@override
+  //void initState() {
+  //  activeScreen = StartScreen(switchScreen);
+  //  super.initState();
+  //}
+
+  void switchScreen() {
+    setState(() {
+      //activeScreen = const QuestionsScreen();
+      activeScreen = 'questions-screen';
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -24,7 +41,9 @@ class _QuizState extends State<Quiz> {
               end: Alignment.bottomRight,
             ),
           ),
-          child: const StartScreen(),
+          child: activeScreen == 'start-screen'
+              ? StartScreen(switchScreen)
+              : const QuestionsScreen(),
         ),
       ),
     );
